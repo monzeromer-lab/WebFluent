345794136e645dd853b73226c4c4f50b9c8e5685
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-04-09 22:43:12 +0200

    fixed: cli issue if no webfluent.app.json is exist

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,24 +1,6 @@
 import { HandleArgs } from "./hundleArgs.ts";
 import { ConfigFileType } from "./types/cli.ts";
 
-/*
-Usage: webfluent [options] [command]
-
-A command-line interface for managing your WebFluent application.
-
-Options:
-  -h, --help          Show this help message and exit.
-
-Commands:
-  generate [options]  Generate a new file or component for your application.
-    Options:
-      -c, --component  Generate a new component.
-      -p, --page       Generate a new page.
-      -s, --style      Generate a new style file.
-  build               Build the WebFluent application.
-  
-Run 'webfluent [command] --help' for more information on a specific command.
- */
 export async function Cli() {
 
   const jsonFilePath = `${Deno.cwd()}/webfluent.app.json`;
@@ -34,7 +16,6 @@ export async function Cli() {
     console.error(
       `No webfluent.app.json file here try webfluent --init to create a new one`
     );
-    Deno.exit(1);
   }
 
   new HandleArgs(Deno.args).run();
